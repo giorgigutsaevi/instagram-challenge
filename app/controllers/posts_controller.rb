902fba4360639bc946fadf4ef6a665e4aa1fd58b
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-22 16:24:34 +0000

    Users can now successfully sign up via an HTML form, unsuccessful signups will get an popup error message with a corresponding error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  has_one_attached :image
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
   # get 'posts/index'
   root "posts#index"
 
   resources :posts
+
+
+  get "signup", to: "users#new"
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

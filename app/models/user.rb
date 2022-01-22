04535630ec304c4d3055053950014ff4a781d613
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-22 13:51:57 +0000

    Added user uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 	validates :username,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 200 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+										uniqueness: {case_sensitive: false}
+	
 
 end

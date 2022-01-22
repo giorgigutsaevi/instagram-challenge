81a384b18a0af16e9eb7e33f9202577f87431103
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-22 13:23:49 +0000

    Added simple tests for user validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,22 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup 
+    @user = User.new(username: "Example", email:"user@example.com", password_digest: "123456")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+  
 end
